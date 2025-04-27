1817facdf9f7a34919d459cf23a7921a03003631
Leo Wu ROG <zwu1@iu.edu>
2025-04-27 15:15:48 -0400

    sync

--- src/run_example.sh
+++ src/run_example.sh
@@ -7,15 +7,18 @@ SCRIPT_PATH="/home/exouser/shared_data/final_project/src/"
 # ==============
 
 # Help message
-if [ "$#" -ne 2 ] || [ "$1" == "--help" ]; then
-    echo "Usage: ./run_na_forcinggen.sh <python_script> <M> <N> <timesteps> <multi_node_flag>"
+if [ "$#" -ne 3 ] || [ "$1" == "--help" ]; then
+    echo "Usage: ./run_na_forcinggen.sh <python_script> <M> <multi_node_flag>"
     echo "  <python_script>: The Python script to run (e.g., NA_forcingGEN_PNetCDF.py)"
+    echo "  <M>: Number of processes"
     echo "  <multi_node_flag>: 0 for single node, 1 for multi-node using hostfile"
     exit 1
 fi
 
 SCRIPT="$SCRIPT_PATH$1"
-MULTI_NODE=$2
+TOTAL_PROCS=$2
+MULTI_NODE=$3
+
 
 # === Conda setup ===
 source "$(conda info --base)/etc/profile.d/conda.sh"

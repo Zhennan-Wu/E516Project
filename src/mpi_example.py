aeb7c09af2c415c2c86561619234d60cbf61dbad
Leo Wu ROG <zwu1@iu.edu>
2025-04-27 15:02:18 -0400

    sync

--- src/mpi_example.py
+++ src/mpi_example.py
@@ -1,9 +1,15 @@
-from mpi4py import MPI
+try:
+    from mpi4py import MPI
+    HAS_MPI4PY = True
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-# Get the rank and size of the current process
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+except ImportError:
+    HAS_MPI4PY = False
+    print("mpi4py is required for this script")
+    rank = -1
+    size =-1
 
 # Print a message from each process
 print(f"Hello from rank {rank} of {size} processes.")

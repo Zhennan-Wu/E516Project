6e3f7ddc1b62b265d3cf2f3e79a67efbea79fdae
Leo Wu ROG <zwu1@iu.edu>
2025-04-27 18:46:16 -0400

    sync

--- src/test_pnetcdf_minimal.py
+++ src/test_pnetcdf_minimal.py
@@ -1,36 +1,36 @@
 from mpi4py import MPI
 import pnetcdf
+from pnetcdf import constants as pnetcdf_const
 
 # Get MPI communicator, rank, size
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-# Check basic info
 if rank == 0:
     print(f"Running with {size} MPI processes")
 
 # Create a new NetCDF file (parallel)
 filename = "testfile.nc"
-cmode = pnetcdf.NC_CLOBBER  # overwrite if exists
+cmode = pnetcdf_const.NC_CLOBBER  # overwrite if exists
 
 # Open file collectively
 ncfile = pnetcdf.File.create(filename, cmode=cmode, comm=comm)
 
 # Define a dimension
 dim_name = "x"
-dim_size = 4  # total size (choose whatever you like)
+dim_size = 4
 ncfile.def_dim(dim_name, dim_size)
 
 # Define a variable
 var_name = "var"
-var_id = ncfile.def_var(var_name, pnetcdf.NC_INT, (dim_name,))
+var_id = ncfile.def_var(var_name, pnetcdf_const.NC_INT, (dim_name,))
 
 # End define mode
 ncfile.enddef()
 
 # Each rank writes one value
-local_value = rank * 10  # or any number you want
+local_value = rank * 10
 
 # Define the write offset
 start = [rank]
